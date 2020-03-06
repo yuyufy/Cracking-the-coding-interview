6767f5887a7240eefdc57f894c7ca82f25086e51
yufeii <smileyufy@163.com>
2020-03-06 11:52:30 +0100

    Update 10.1_softed_merged.cpp

--- 10.1_softed_merged.cpp
+++ 10.1_softed_merged.cpp
@@ -1,1 +1,28 @@
+/*
+Problem Statement:
+You are given two sorted arrays, A and B, where A has a large enough buffer at the end to hold B.
+Write a method to merge B into A in sorted order.
 
+Solution : 
+*/
+class Solution{
+ public : 
+  void mergeTwoarrays(vector<int>& A, vector<int>& B, int lastAIndex, int lastBIndex) {
+     /* Index of last location of merged array */
+      int mergeIndex = lastAIndex + lastBIndex-1;
+      while(lastBIndex>0)
+      {
+        if (lastindexA >= 0 && A[lastindexA] > B[lastindexB]) 
+        {
+            A[mergeIndex] = A[lastindexA];
+            lastAindex--;
+        }
+        else
+        {
+            A[mergeIndex] = B[lastindexA];
+            lastBindex--;
+        }
+        mergeIndex--;
+      }
+  }
+}
