7e2ca573f3384d9aa21746500f026ce818b88767
yufeii <smileyufy@163.com>
2020-03-06 14:26:17 +0100

    Create 10.5sparseSearch.cpp

---
+++ 10.5sparseSearch.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+class Solution{
+public : 
+  int sparseSearch(const std::vector<std::string>& array, const std::string& query)
+  {
+  
+  }
+};
