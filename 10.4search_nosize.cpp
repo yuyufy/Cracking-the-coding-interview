1ffa5393518ee9701350611634f365a164bfded3
yufeii <smileyufy@163.com>
2020-03-06 14:38:55 +0100

    Create 10.4search_nosize.cpp

---
+++ 10.4search_nosize.cpp
@@ -0,0 +1,18 @@
+/*
+probleme : Given an integer array sorted in ascending order, write a function to search target in nums.  If target exists, then return 
+its index, otherwise return -1. However, the array size is unknown to you. You may only access the array using an ArrayReader interface, 
+where ArrayReader.get(k) returns the element of the array at index k (0-indexed).
+You may assume all integers in the array are less than 10000, and if you access the array out of bounds, ArrayReader.get will return 2147483647.
+class ArrayReader {
+    public:
+      int get(int index);
+ };
+
+*/
+
+class Solution {
+public:
+    int search(const ArrayReader& reader, int target) {
+        
+    }
+};
