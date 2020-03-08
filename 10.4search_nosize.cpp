491986d73feb6dcf0142fa1fe95ad330de70e1ef
yufeii <smileyufy@163.com>
2020-03-08 11:54:51 +0100

    Update 10.4search_nosize.cpp

--- 10.4search_nosize.cpp
+++ 10.4search_nosize.cpp
@@ -13,6 +13,24 @@ class ArrayReader {
 class Solution {
 public:
     int search(const ArrayReader& reader, int target) {
-        
+        int pivot = 0, left = 0, right = 1;
+        if(reader.get(0)==target) return 0;
+        while(reader.get(right)<target)
+        {
+            left = right;
+            right<<=1;
+        }
+        while(left<=right)
+        {
+            pivot = left + ((right-left)>>1);
+            int num = reader.get(pivot);
+            if(num==target) return pivot;
+            else if(num<target)
+                left = pivot+1;
+            else
+                right = pivot-1;
+            
+        }
+        return -1;
     }
 };
