019205222c689b4557690b0af37219db43601da4
yufeii <smileyufy@163.com>
2020-03-08 17:00:36 +0100

    Create 8.4power_set.cpp

---
+++ 8.4power_set.cpp
@@ -0,0 +1,17 @@
+/*
+Probleme : Given a set of distinct integers, nums, return all possible subsets (the power set).
+There are 2^n subsets for a given set S of N size
+Example : S = {0,1,2}
+*/
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        for(int i=0 ; i<nums.size();i++)
+        {
+            vector<int> subset;
+            
+        }
+        return res;
+    }
+};
