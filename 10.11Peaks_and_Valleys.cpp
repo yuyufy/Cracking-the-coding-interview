fbea14357f6104807bd5929b4ee9dda3eadd6cec
yufeii <smileyufy@163.com>
2020-03-08 13:36:15 +0100

    Create 10.11Peaks_and_Valleys.cpp

---
+++ 10.11Peaks_and_Valleys.cpp
@@ -0,0 +1,10 @@
+/*
+PROBLEM:
+   Given an array of integers, sort the array into one of alternating peaks and valleys
+   where a peak is an integer greater than or equal to its neighbors and a valley is an
+   integer less than or equal to its neighbors.
+ 
+   TEST CASES:
+   {5, 3, 1, 2, 3} --> {5, 1, 3, 2, 3}
+   {5, 8, 6, 2, 3, 4, 6} --> {5, 8, 2, 6, 3, 6, 4};
+*/
