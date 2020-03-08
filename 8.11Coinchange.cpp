0a32ed0d83551a9ea7d8d24ed853a08c78192402
yufeii <smileyufy@163.com>
2020-03-08 20:53:28 +0100

    Update 8.11Coinchange.cpp

--- 8.11Coinchange.cpp
+++ 8.11Coinchange.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
 
+class Solution{
+  public : 
+   int coinChange(vector<int>& coins, int amount) 
+   {
+     int max = amount+1;
+     vector<int> dp(max,max);
+     for(int i = 1; i<=amount;i++)
+     {
+       for(int j = 0; j<coins.size();j++)
+       {
+         if(coins[j]<=i)
+           dp[i] = min(dp[i],dp[i-coins[j]]+1);
+       }
+     }
+     return dp[amount]>amount? -1 : dp[amount];
+   }
+};
