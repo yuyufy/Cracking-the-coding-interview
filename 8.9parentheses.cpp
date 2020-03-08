caa3a6b72023d73df8c28fe17480d9b1c9060264
yufeii <smileyufy@163.com>
2020-03-08 21:47:30 +0100

    Update 8.9parentheses.cpp

--- 8.9parentheses.cpp
+++ 8.9parentheses.cpp
@@ -2,7 +2,22 @@
 #include <string>
 
 using namespace std;
-void print_all_parentheses_interna(int count, size_t opened, size_t closed, std::string arangement)
-{
 
+void all_parentheses(int count, int opened, int closed, string arangement)
+{
+    if(closed>opened) return;
+    if(opened>count) return;
+    else if(arangement.size()==2*count)
+      cout<<arangement<<endl;
+    all_parentheses(count, opened+1, closed, arangement+"(");
+   all_parentheses(count, opened, closed+1, arangement+")");
+}
+
+void print_all_parentheses(int count){
+  all_parentheses(count, 0, 0, "");
+}
+
+int main(int argc, const char* argv[]) {
+  print_all_parentheses(3);
+  return 0;
 }
