df9495f097667b12d460390787d45fec4c240942
yufeii <smileyufy@163.com>
2020-03-08 16:48:42 +0100

    Update 8.3MagicIndex.cpp

--- 8.3MagicIndex.cpp
+++ 8.3MagicIndex.cpp
@@ -3,8 +3,38 @@
 #include<algorithm>
 using namespace std;
 
-int magicIndex(vector<int>& array)
-{
-  int res = 0;
-  return res;
+class Solution{
+  //Solution 1
+  int helper(vector<int>& array,int start, int end)
+  {
+     if(start<0||end>array.size()) return -1;
+     if(start>end) return -1;
+     int mid = start + (end - start) /2;
+     if(mid == array[mid]) return mid;
+     else
+     {
+        if(array[mid]>mid) return helper(array,start,mid-1);
+        else return helper(array,mid+1,end);
+     }
+  }
+    //Solution 2
+  int helper(vector<int>& array,int start, int end)
+  {
+     if(start<0||end>array.size()) return -1;
+     if(start>end) return -1;
+     int mid = start + (end - start) /2;
+     if(mid == array[mid]) return mid;
+     else
+     {
+        // search left
+        int left =  helper(array,start,min(mid-1,array[mid]));
+        if(left>0) return left;
+        return helper(array,max(mid+1,array[mid]),end);
+     }
+  }
+  int magicIndex(vector<int>& array)
+  {
+      if(array.size() == 0) return -1;
+      return helper(array,0,array.size()-1);
+  };
 }
