5047352f8689fa9c3a2d29975145eefa2709448c
yufeii <smileyufy@163.com>
2020-03-08 14:08:06 +0100

    Update 8.1Triplestep.cpp

--- 8.1Triplestep.cpp
+++ 8.1Triplestep.cpp
@@ -1,1 +1,25 @@
+#include <vector>
+#include <iostream>
 
+using namespace std;
+
+class Solution{
+  int helper(int n, vector<int>& memo)
+  {
+    if(n==0) return 0;
+    if(n==1) return 1;
+    if(n==2) return 2;
+    if(n==3) return 4;
+    if(memo[n]==0) 
+      memo[n] = helper(n-3,memo) + helper(n-2,memo) + helper(n-1,memo);
+    return memo[n];
+  }
+  int tripleStep(int n)
+  {
+    //all one step 1
+    //all one step (n-2) +2
+    //all one step (n-3)+3
+    vector<int> memo(n+1,0);
+    return helper(n,memo);
+  };
+}
